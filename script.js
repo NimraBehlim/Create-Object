aebfe74bc36f0855afe61630ff68604a3c1928f6
NimraBehlim <165383296+NimraBehlim@users.noreply.github.com>
2024-10-04 18:41:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+var person1 = {
+    firstName: "Ellis",
+    lastName: "Parker",
+    age: "30",
+    eyeColor: "Blue"
+};
+
+document.getElementById("div1").innerHTML = "FirstName: " + person1.firstName + "<br>" + "LastName: " + person1.lastName + "<br>" + "Age:" + person1.age + "<br>" + "Eyecolor: " + person1.eyeColor
+
+var fruitColors = {
+    mango: "Yellow",
+    orange: "Orange",
+    stawberry: "Red",
+    grappes: "Green",
+    apple: "Red"
+};
+
+document.getElementById("div2").innerHTML = "Mango: " + fruitColors.mango + "<br>" + "Orange: " + fruitColors.orange + "<br>" + "Stawberry: " + fruitColors.stawberry + "<br>" + "Grappes: " + fruitColors.grappes + "<br>" + "Apple: " + fruitColors.apple;
+
+var languages = {
+    kashmir: "Kashmiri",
+    punjab: "Punjabi",
+    balochistan: "Balochi",
+    sarhad: "Pashto"
+};
+
+document.getElementById("div3").innerHTML = "Kashmir: " + languages.kashmir + "<br>" + "Punjab: " + languages.punjab + "<br>" + "Baluchistan: " + languages.balochistan + "<br>" + "Sarhad: " + languages.sarhad;
+
+var laptop = {
+    brand: "Apple",
+    model: "MacBook Pro",
+    screenSize: "14 inch",
+    processor: "M1 Pro",
+    ram: "16GB",
+    weight: "3.5 lbs"
+};
+
+document.getElementById("div4").innerHTML = "Brand: " + laptop.brand + "<br>" + "Modle: " + laptop.model + "<br>" + "ScreenSize: " + laptop.screenSize + "<br>" + "Processor: " + laptop.processor + "<br>" + "Ram: " + laptop.ram + "<br>" + "Weight: " + laptop.weight;
+
+var fruitJuice = {
+    red: "Pomegrant",
+    yellow: "Mango",
+    purple: "Red grappes",
+    orange: "Orange",
+    White: "Lychee"
+};
+
+document.getElementById("div5").innerHTML = "Red: " + fruitJuice.red + "<br>" + "Yellow: " + fruitJuice.yellow + "<br>" + "Purple: " + fruitJuice.purple + "<br>" + "Orange: " + fruitJuice.orange + "<br>" + "White: " + fruitJuice.White;
+
+var Drink = {
+    company: "Pepsi",
+    color: "Black",
+    taste: "Fizzy",
+    container: "Can"
+};
+
+document.getElementById("div6").innerHTML = "Company: " + Drink.company + "<br>" + "Color: " + Drink.color + "<br>" + "Taste: " + Drink.taste + "<br>" + "Container: " + Drink.container;
+
+var Quran = {
+    title: "The Holy Quran",
+    language: "Arabic",
+    binding: "Leather",
+    price: "$15.99",
+    surahs: 114
+};
+
+document.getElementById("div7").innerHTML = "Title: " + Quran.title + "<br>" + "Language: " + Quran.language + "<br>" + "Binding: " + Quran.binding + "<br>" + "Price: " + Quran.price + "<br>" + "Surahs: " + Quran.surahs;
+
+var Mobile = {
+    charger: "Type C",
+    Cover: "Transparent",
+    Sim: "Ufone",
+    model: "Samsung Galaxy ",
+    color: "Purple"
+};
+
+document.getElementById("div8").innerHTML = "Charger: " + Mobile.charger + "<br>" + "Cover: " + Mobile.Cover + "<br>" + "Sim: " + Mobile.Sim + "<br>" + "Model: " + Mobile.model + "<br>" + "Color: " + Mobile.color;
+
+var NationalFlag = {
+    country: "Pakistan",
+    colors: ["Green", "White"],
+    dateOfInvention: "August 11, 1947",
+    designer: "Syed Amir-uddin Kedwaii"
+};
+
+document.getElementById("div9").innerHTML = "Country: " + NationalFlag.country + "<br>" + "Colors: " + NationalFlag.colors + "<br>" + "Date of invetion: " + NationalFlag.dateOfInvention + "<br>" + "Designer: " + NationalFlag.designer;
+
+var Dress = {
+    brand: "J.",
+    fabric: "Lawn",
+    color: "Light colors",
+    piece: "3 piece"
+};
+
+document.getElementById("div10").innerHTML = "Brand: " + Dress.brand + "<br>" + "Fabric: " + Dress.fabric + "<br>" + "Color: " + Dress.color + "<br>" + "Piece: " + Dress.piece;
